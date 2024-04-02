db542d0c77b5c012027fa7860fe5f8b9220fe265
Patrickvddd <patrick.van.daal@gmail.com>
2024-04-02 16:42:03 +0000

    full days work

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,123 @@
-console.log("rock paper scissors");
+
+let choices = ["rock", "paper", "scissors"];
+let playerId = document.getElementById("playername");
+let playerScore = document.getElementById("playerscore");
+let computerScore = document.getElementById("computerscore");
+let winner = document.getElementById("winner");
+let compSelection = document.getElementById('computerchoice');
+let started = false
+let playerScoreNum = 0
+let compScoreNum = 0
+function startGame (playerChoice){
+        if(started){
+                return
+        }
+        winner.style.visibility = 'hidden';
+        compSelection.style.visibility = 'hidden';
+        started = true 
+        let playerSelection =document.getElementById("playerchoice");
+        if (playerChoice == "rock") {
+          playerSelection.src = "assets/images/fist.png";
+        }else if (playerChoice == "paper") {
+         playerSelection.src = "assets/images/paper.png";
+        }else if (playerChoice =="scissors") {
+         playerSelection.src = "assets/images/scissors.png";
+        }
+        playerSelection.style.visibility = 'visible';
+
+        
+
+        setTimeout(function(){    
+        let computerChoice = choices[Math.floor(Math.random() * 3)];
+        console.log(computerChoice)
+
+        
+        if (computerChoice == "rock") {
+        compSelection.src = "assets/images/fist.png";
+        }else if (computerChoice == "paper") {
+        compSelection.src = "assets/images/paper.png";
+        }else if (computerChoice =="scissors") {
+        compSelection.src = "assets/images/scissors.png";
+        }
+        compSelection.style.visibility = 'visible';
+        
+        let result="";
+        if (playerChoice === computerChoice){
+                result = draw();
+                
+        
+        }else {
+                switch(playerChoice){
+                        case "rock": result = (computerChoice === "scissors") ? win() : lose();
+                        break;
+                        case "paper": result = (computerChoice === "rock") ? win() : lose();
+                        break;
+                        case "scissors": result = (computerChoice === "paper") ? win() : lose();
+                        break;
+                }
+
+        }
+        
+        
+}, 1000)
+}
+
+
+
+function win(){
+        
+        setTimeout(() =>{
+
+        console.log("winner")
+        winner.style.visibility = 'visible';
+        winner.src = "assets/images/win.png";
+        started = false
+        document.getElementById("playerscore").innerText = ++playerScoreNum;
+        if (playerScoreNum === 10){
+                resetScore();
+                alert("YOU WIN")
+                
+        }},1000)
+       
+}
+
+function lose(){
+        setTimeout(() =>{
+        console.log("loser")
+        winner.style.visibility = 'visible';
+       winner.src = "assets/images/lose.png"
+       started = false
+        document.getElementById("computerscore").innerText = ++compScoreNum;
+        if (compScoreNum === 10){
+                resetScore();
+                alert("YOU LOSE")
+        }},1000)
+        
+}
+function resetScore(){
+        document.getElementById("playerscore").innerText = "0"
+        document.getElementById("computerscore").innerText = "0"
+        playerScoreNum = 0
+        compScoreNum = 0
+        
+}
+function draw(){
+        setTimeout(() =>{
+        winner.style.visibility = 'visible';
+        console.log("draw")
+        winner.src = "assets/images/draw.png"
+        started = false
+},1000)
+
+}
+
+function onSubmit(){
+        
+        let user = document.getElementById("username");
+        let userName = user.value;
+        document.getElementById("playername").innerHTML = playername;
+        playername.textContent = userName;
+        document.getElementById("username").value = "";
+}
+let submitButton = document.getElementById('submit');
+submit.addEventListener('click', onSubmit);
